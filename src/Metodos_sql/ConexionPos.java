5479789e6766d5d318d0be7c13151616ca391875
Jeravima <64504893+Jeravima@users.noreply.github.com>
2020-05-16 20:28:49 -0500

    Modificacion ConexionPos

--- src/Metodos_sql/ConexionPos.java
+++ src/Metodos_sql/ConexionPos.java
@@ -4,127 +4,69 @@
  * and open the template in the editor.
  */
 package Metodos_sql;
+import com.sun.rowset.CachedRowSetImpl;
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.sql.rowset.CachedRowSet;
 /**
  *
  * @author Estructura de Datos D1
  */
 public class ConexionPos {
-    public static ConexionBD conexion = new ConexionBD();
-    
-    public static PreparedStatement sentencia_preparada;
-    public static ResultSet resultado;
-    public static String sql;
-    public static int resultado_numero=0;
-    
-    
-    public int guardar(String nombre, String apellidos, String correo , String contraseña){
-        
-        int resultado =0;
-        Connection conexion = null;
-        
-        String sentencia_guardar = ("INSERT INTO login_bd(nombre,apellidos,correo,contraseña) VALOES (?,?,?,?)");
-    
-    
-        try{
-        
-            conexion = ConexionBD.conectar();
-            sentencia_preparada = conexion.prepareStatement(sentencia_guardar);
-            sentencia_preparada.setString(1,nombre);
-            sentencia_preparada.setString(2,apellidos);
-            sentencia_preparada.setString(3,correo);
-            sentencia_preparada.setString(4,contraseña);
-            
-            resultado = sentencia_preparada.executeUpdate();
-            sentencia_preparada.close();
-                
-        
-        }catch(Exception e){
-        
-            System.out.println(e);
-        
-        
-        
-        }return resultado;
-    }
-    
-    public static String buscarNombre (String correo){
-        
-        String busqueda_nombre = null;
-        Connection conexion = null;
-
-        
-                try{
-        
-            conexion = ConexionBD.conectar();
-            String sentencia_buscar=("SELECT nombre,apellidos FROM login_bd WHERE correo = '"+ correo +"'");
-
-            if(resultado.next()){
-            
-            String nombre = resultado.getString("nombre");
-            String apellidos = resultado.getString("apellidos");
-            busqueda_nombre = (nombre +""+ apellidos);
-            
+    public CachedRowSet Function(String sql)
+    {
+            try
+            {
+                    Class.forName("org.postgresql.Driver");
+ 
+                    String url = "jdbc:postgresql://127.0.0.1:8080/postgres";
+                    Connection con = DriverManager.getConnection("jdbc:mysql://localhost/unipaz", "root","");
+                    Statement s = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,
+                                                      ResultSet.CONCUR_READ_ONLY);
+ 
+                    ResultSet rs= s.executeQuery(sql);
+ 
+                    CachedRowSet crs = new CachedRowSetImpl();
+                    crs.populate(rs);
+ 
+                    rs.close();
+                    s.close();
+                    con.close();
+ 
+                    return crs;
+ 
             }
-            
-            conexion.close();
-        
-        }catch(Exception e){
-        
-            System.out.println(e);
-        
-        
-        
-        }
-                return busqueda_nombre;
-    }
-                
-    public static String buscarUsarioregistrado (String correo,String contraseña){
-        
-        String busqueda_usuario = null;
-        Connection conexion = null;
-               try{
-        
-            conexion = ConexionBD.conectar();
-            String sentencia_buscar_usuario =("SELECT nombre,correo,contraseña FROM login_bd WHERE correo = '"+ correo +"' && contraseña"+contraseña+"'");
-            sentencia_preparada = conexion.prepareStatement(sentencia_buscar_usuario);
-            resultado = sentencia_preparada.executeQuery();
-            if(resultado.next()){   
-                busqueda_usuario = "usuario encontrado"; 
-            }else{
-                busqueda_usuario = "usuario no encontrado"; 
+            catch(Exception e)
+            {
+                   System.out.println(e.getMessage());
             }
-            
-      
-            sentencia_preparada.close();
-                
-        
-        }catch(Exception e){
-        
-            System.out.println(e);
-        
-        
-        
-        }
-        return busqueda_usuario;
-        
-        
-        
-        
-        
-        
-        
+ 
+            return null;
     }
-
-    public CachedRowSet Function(String select_id_estudiantes_nombre_estudiantesa) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+ 
+    public void StoreProcedure(String sql)
+    {
+ 
+            try
+            {
+                    Class.forName("org.postgresql.Driver");
+ 
+                    String url = "jdbc:postgresql://127.0.0.1:8080/postgres";
+                    Connection con = DriverManager.getConnection(url, "usuario","contraseña");
+                    Statement s = con.createStatement();
+ 
+                    s.execute(sql);
+ 
+                    s.close();
+                    con.close();
+ 
+            }
+            catch(Exception e)
+            {
+                   System.out.println(e.getMessage());
+            }
     }
-    
-    
-    
-    
-    
 }
